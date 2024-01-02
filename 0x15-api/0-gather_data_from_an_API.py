98caf7feaadfb1d01c0b56e5276daf80f7c1b83e
zelalemng <zedneg@gmail.com>
2024-01-02 07:36:52 -0800

    task_0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,10 +16,13 @@ if __name__ == "__main__":
     todos_response = requests.get(url + "todos", params=params)
     todos = todos_response.json()
     completed = []
+
     for todo in todos:
         if todo.get("completed") is True:
             completed.append(todo.get("title"))
+
     print("Employee {} is done with tasks({}/{})".format(user.get("name"),
         len(completed), len(todos)))
+
     for complete in completed:
         print("\t {}".format(complete))
