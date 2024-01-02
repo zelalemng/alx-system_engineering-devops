e3bfd7d281435466780e0a880ff3ae4bb57cdda4
zelalemng <zedneg@gmail.com>
2024-01-02 10:48:55 -0800

    task_0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,17 +9,16 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    user_todo_url =
-    "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+    user_todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
     resp = req.get(url).json()
-    user_name = resp.get("name")
-    count = 0
+    EMPLOYEE_NAME = resp.get("name")
+    NUMBER_OF_DONE_TASKS = 0
     user_todos = req.get(user_todo_url).json()
-    total_todos = len(user_todos)
+    TOTAL_NUMBER_OF_TASKS = len(user_todos)
     for todo in user_todos:
         if todo.get("completed"):
-            count += 1
-    print(f"Employee {user_name} is done with tasks({count}/{total_todos})")
+            NUMBER_OF_DONE_TASKS += 1
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS})")
     [print(
         f"\t {todos.get('title')}"
         ) for todos in user_todos if todos.get("completed")]
